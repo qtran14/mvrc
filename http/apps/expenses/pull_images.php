f2a92a851d9f75f90033b7b684bcb0c02ca16b5e
Quoc Tran <quoctran.cs.titan@gmail.com>
2016-06-26 14:34:06 -0500

    add link to the image

--- http/apps/expenses/pull_images.php
+++ http/apps/expenses/pull_images.php
@@ -5,6 +5,6 @@ $loginInfo = Session::get('user');
 $images = (new Expenses_Pictures)->belongsTo($hash);
 
 $imageArray = [];
-foreach ( $images as $image ) $imageArray[] = '<li class="mix category-animal" style="display: inline-block;"><div class="gallery-item rounded shadow"><img src="/accounts/' . $loginInfo['account_hash'] . '/images/thumbnail/' . $image['name'] . '" width="150px" /></div></li>';
+foreach ( $images as $image ) $imageArray[] = '<li class="mix category-animal" style="display: inline-block;"><div class="gallery-item rounded shadow"><a target="_blank" class="gallery-img" href="/accounts/' . $loginInfo['account_hash'] . '/images/large/' . $image['name'] . '"><img src="/accounts/' . $loginInfo['account_hash'] . '/images/thumbnail/' . $image['name'] . '" width="150px" /></a></div></li>';
 die(json_encode(['images' => $imageArray]));
 ?>
