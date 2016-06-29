efb36caca731af83ef2602cc3b053e61017380d3
Quoc Tran <quoctran.cs.titan@gmail.com>
2016-06-28 19:48:20 -0500

    clean up mass upload

--- http/apps/expenses/store_upload.php
+++ http/apps/expenses/store_upload.php
@@ -50,21 +50,6 @@ if ( isset($post['upload']) && $post['upload'] == '1' ) {
 	}
 
 
-	// $inputData = [
-	// 	'hash' => getUUID(60, getDB(), 'expenses', 'hash'),
-	// 	'account_hash' => $loginInfo['account_hash'],
-	// 	'on_date' => dbDate($post['expense_date']),
-	// 	'name' => encodeQuote($post['expense_name']),
-	// 	'category_id' => $post['expense_category'],
-	// 	'status_id' => 1,
-	// 	'amount' => $post['expense_total'],
-	// 	'description' => encodeQuote($post['expense_notes']),
-	// 	'created_by' => $loginInfo['id'],
-	// ];
-
-	// $expense = new Expenses;
-	// $expense->save($inputData);
-
 	Session::setFlash('success', "Expense Added.");
 	redirect('/expenses');
 }
--- http/apps/expenses/upload.php
+++ http/apps/expenses/upload.php
@@ -5,18 +5,12 @@ $data['active_expense_main'] = 'active';
 
 $htmlPluginCSS  = [
 	'/assets/global/plugins/bower_components/bootstrap-fileupload/css/bootstrap-fileupload.min',
-	// '/assets/global/plugins/bower_components/datatables/css/datatables.responsive'
 ];
 
 $htmlPluginJS   = [
 	'/assets/global/plugins/bower_components/bootstrap-fileupload/js/bootstrap-fileupload.min',
-	// '/assets/global/plugins/bower_components/datatables/js/dataTables.bootstrap',
-	// '/assets/global/plugins/bower_components/datatables/js/datatables.responsive'
 ];
 
-// $htmlCustomJS   = [
-// 	'/js/expenses/history.list'
-// ];
 
 renderView('expenses/upload');
 ?>
