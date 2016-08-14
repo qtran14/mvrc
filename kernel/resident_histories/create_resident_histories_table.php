90bc76a105db02aeb14438fce969d5395c667f40
Quoc Tran <quoctran.cs.titan@gmail.com>
2016-08-14 10:04:19 -0500

    adjust the db

--- kernel/resident_histories/create_resident_histories_table.php
+++ kernel/resident_histories/create_resident_histories_table.php
@@ -35,9 +35,9 @@ $sql = "CREATE TABLE {$table} (
 echo $sql;
 $db->rawQuery($sql);
 
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2008-07-01', '2009-06-30', '10631 Tibbs Cir', 'Apt 2', 'Garden Grove', 'CA', '92840')");
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `city`, `state`, `zip_code`) VALUES ('1', '2009-07-01', '2013-03-01', '12441 La Linda Cir', 'Garden Grove', 'CA', '92841')");
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2013-03-01', '2013-10-01', '446 W 10th St', 'Apt 1', 'Erie', 'PA', '16502')");
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2013-10-01', '2014-02-01', '3615 Brandes St', 'Front', 'Erie', 'PA', '16504')");
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `city`, `state`, `zip_code`) VALUES ('1', '2014-02-01', '2015-09-01', '12441 La Linda Cir', 'Garden Grove', 'CA', '92841')");
-$db->rawQuery("INSERT INTO `all_in_one_dev`.`resident_histories` (`resident_member_id`, `start_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2015-09-01', '1610 Camellia Dr', 'D2', 'Munster', 'IN', '46321')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2008-07-01', '2009-06-30', '10631 Tibbs Cir', 'Apt 2', 'Garden Grove', 'CA', '92840')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `city`, `state`, `zip_code`) VALUES ('1', '2009-07-01', '2013-03-01', '12441 La Linda Cir', 'Garden Grove', 'CA', '92841')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2013-03-01', '2013-10-01', '446 W 10th St', 'Apt 1', 'Erie', 'PA', '16502')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2013-10-01', '2014-02-01', '3615 Brandes St', 'Front', 'Erie', 'PA', '16504')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `end_date`, `address_1`, `city`, `state`, `zip_code`) VALUES ('1', '2014-02-01', '2015-09-01', '12441 La Linda Cir', 'Garden Grove', 'CA', '92841')");
+$db->rawQuery("INSERT INTO `resident_histories` (`resident_member_id`, `start_date`, `address_1`, `address_2`, `city`, `state`, `zip_code`) VALUES ('1', '2015-09-01', '1610 Camellia Dr', 'D2', 'Munster', 'IN', '46321')");
